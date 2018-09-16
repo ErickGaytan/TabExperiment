b566d2ca190c747859232c02b4a2a4ff6671bd0b
ErickGaytan <erick.g@hotmail.com>
2018-09-16 18:26:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/user/tabexperiment/TabFragment1.kt
@@ -0,0 +1,22 @@
+package com.example.user.tabexperiment
+
+import android.os.Bundle
+import android.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class TabFragment1 : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.tab_fragment1, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/user/tabexperiment/TabFragment2.kt
@@ -0,0 +1,23 @@
+package com.example.user.tabexperiment
+
+import android.os.Bundle
+import android.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class TabFragment2 : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.tab_fragment2, container, false)
+    }
+
+
+}
